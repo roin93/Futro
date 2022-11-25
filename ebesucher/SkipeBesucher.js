e4ea925b74536c9cc6b66898983eb14d6f2f58c0
Blinchik91 <70434936+Blinchik91@users.noreply.github.com>
2022-11-25 16:40:47 +0100

    Create SkipeBesucher.js

---
+++ ebesucher/SkipeBesucher.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         SkipeBesucher
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Klickt auf jetzt surfen und überspringt die werbelose Seite
+// @author       Blin4ik
+// @match        https://www.ebesucher.de/surfbar/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+
+function FindButtons() {
+    var buttonMatches = [],
+        ButtonElems = document.getElementsByTagName("button"),
+        iframes = document.getElementsByTagName('iframe'),
+        l = ButtonElems.length,
+        m = iframes.length, i, j;
+    for( i=0; i<l; i++) buttonMatches[i] = ButtonElems[i];
+    for( j=0; j<m; j++) {
+        ButtonElems = iframes[j].contentDocument.getElementsByTagName("button");
+        l = ButtonElems.length;
+        for( i=0; i<l; i++) buttonMatches.push(ButtonElems[i]);
+    }
+    return buttonMatches;
+}
+
+(function() {
+    'use strict';
+     var surflink = "https://www.ebesucher.de/surfbar/[surflink]"  // dein surflink
+     setTimeout(function() {
+     var url = document.URL;
+        if (url.includes("ebesucher.de")) {
+           var buttons = FindButtons();
+            if (buttons[0].innerText == "Jetzt surfen!")
+            {
+             buttons[0].click();
+            }
+            else
+            {
+                window.location.assign(surflink);
+            }
+        }
+    }, 5000);
+})();
