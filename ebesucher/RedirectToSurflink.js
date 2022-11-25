0405101e1dc482b5197277936512509b118b681d
Blinchik91 <70434936+Blinchik91@users.noreply.github.com>
2022-11-25 16:44:29 +0100

    Create RedirectToSurflink.js

---
+++ ebesucher/RedirectToSurflink.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         RedirectToSurflink
+// @version      1.0
+// @description  nach 10 minuten wird automatisch auf den surflink navigiert
+// @author       Blin4ik
+// @match        https://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cnoops.de
+// @grant        none
+// ==/UserScript==
+
+(function() {
+     'use strict';
+    var surflink = "https://www.ebesucher.de/surfbar/[surflink]" // dein surflink
+    window.addEventListener('load', function() {
+     var currentUrl = document.URL;
+     setTimeout(function() {
+     var activeUrl = document.URL;
+        if (currentUrl == activeUrl) {
+          window.location.assign(surflink);
+        }
+    }, 600000); // wait 10 minutes
+}, false);
+})();
