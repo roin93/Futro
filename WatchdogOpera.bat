7e0d9d72b4f6921fa2141bb384802c26535c3240
Blinchik91 <70434936+Blinchik91@users.noreply.github.com>
2022-12-02 08:23:11 +0100

    Create WatchdogOpera.bat

---
+++ WatchdogOpera.bat
@@ -0,0 +1,30 @@
+:: Description: This batch file will restart opera.exe every 30 minutes 
+:: Authors: roin93 & Blinchik
+
+@ECHO OFF
+title Watchdog for opera
+SET P=opera.exe
+
+:Beginning
+ECHO %date% %time%- Every 30 minutes this script will close opera.exe and will start it again.
+:: List all Processes which contain P, but do not show in terminal
+TASKLIST | FINDSTR /I "%P%" > NUL
+
+IF ERRORLEVEL 1 (GOTO :Starting)
+:: Sleep for 1800 seconds / 30 minutes
+timeout /t 1800 /nobreak > NUL
+
+ECHO %date% %time% - Close Opera Browser
+:: After Sleeping, now close opera and wait for a restart
+Powershell -C "Get-Process opera | ForEach-Object { $_.CloseMainWindow() | Out-Null}" > NUL
+timeout /t 10 /nobreak > NUL
+
+:: kill dead processes to clear memory
+taskkill /IM opera.exe /F 2> nul
+timeout /t 10 /nobreak > NUL
+
+:Starting
+ECHO %date% %time% - Starting Opera Browser
+Start "[FullPath]\opera.exe" "https://www.ebesucher.de/surfbar/[surflink]"
+timeout /t 10 /nobreak > NUL
+GOTO :Beginning
