59ed0ad941ceb21450c387af50ecae8e255939d9
Blinchik91 <70434936+Blinchik91@users.noreply.github.com>
2022-12-02 08:19:02 +0100

    Create WatchdogMsEdge.bat

---
+++ WatchdogMsEdge.bat
@@ -0,0 +1,30 @@
+:: Description: This batch file will restart msedge.exe every 30 minutes 
+:: Authors: roin93 & Blinchik
+
+@ECHO OFF
+title Watchdog for MS Edge
+SET P=msedge.exe
+
+:Beginning
+ECHO %date% %time% - Every 30 minutes this script will close msedge.exe and will start it again.
+:: List all Processes which contain P, but do not show in terminal
+TASKLIST | FINDSTR /I "%P%" > NUL
+
+IF ERRORLEVEL 1 (GOTO :Starting)
+:: Sleep for 1800 seconds / 30 minutes
+timeout /t 1800 /nobreak > NUL
+
+ECHO %date% %time% - Close Edge browser
+:: After Sleeping, now close msedge and wait for a restart
+Powershell -C "Get-Process msedge | ForEach-Object { $_.CloseMainWindow() | Out-Null}" > NUL
+timeout /t 10 /nobreak > NUL
+
+:: Kill dead processes to clear memory
+taskkill /IM msedge.exe /F 2> nul
+timeout /t 10 /nobreak > NUL 
+
+:Starting
+ECHO %date% %time% - Starting Edge Browser
+Start msedge "https://www.ebesucher.de/surfbar/[surfbar]"
+timeout /t 10 /nobreak > NUL
+GOTO :Beginning
