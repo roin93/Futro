5595e609bf64fed54f7586769a65717ace840224
Roin93 <24556173+roin93@users.noreply.github.com>
2022-12-19 17:25:58 +0100

    Added variable waittime
    It is easier to change the wanted waittime now.

--- ebesucher/WatchdogMsEdge.bat
+++ ebesucher/WatchdogMsEdge.bat
@@ -5,6 +5,10 @@
 @ECHO OFF
 title Watchdog for MS Edge
 SET P=msedge.exe
+:: Waittime in seconds
+SET WAITTIME=1800
+:: Seconds to wait after heavy commands
+SET GIVETIME=10
 
 :Beginning
 ECHO %date% %time% - Every 30 minutes this script will close msedge.exe and will start it again.
@@ -12,20 +16,20 @@ ECHO %date% %time% - Every 30 minutes this script will close msedge.exe and will
 TASKLIST | FINDSTR /I "%P%" > NUL
 
 IF ERRORLEVEL 1 (GOTO :Starting)
-:: Sleep for 1800 seconds / 30 minutes
-timeout /t 1800 /nobreak > NUL
+:: Sleep for seconds specified in waittime
+timeout /t %WAITTIME% /nobreak > NUL
 
 ECHO %date% %time% - Close Edge browser
 :: After Sleeping, now close msedge and wait for a restart
 Powershell -C "Get-Process msedge | ForEach-Object { $_.CloseMainWindow() | Out-Null}" > NUL
-timeout /t 10 /nobreak > NUL
+timeout /t %GIVETIME% /nobreak > NUL
 
 :: Kill dead processes to clear memory
 taskkill /IM msedge.exe /F /T 2> nul
-timeout /t 10 /nobreak > NUL 
+timeout /t %GIVETIME% /nobreak > NUL 
 
 :Starting
 ECHO %date% %time% - Starting Edge Browser
 Start msedge "https://www.ebesucher.de/surfbar/[surfbar]"
-timeout /t 10 /nobreak > NUL
+timeout /t %GIVETIME% /nobreak > NUL
 GOTO :Beginning
